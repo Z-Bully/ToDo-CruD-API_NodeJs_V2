6a8aeb946bd2e14eaee8ee5b5b46e7b2bda9009d
Z-Bully <drenismice@gmail.com>
2023-03-13 10:59:45 +0100

    Read Todos

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const http = require('http')
-const fs = require('fs')
+// import {} from 'node:fs'
+// import {} from 'module'
+const todos = require('./data/Todos')
+
+const server = http
+  .createServer(async (req, res) => {
+    // Display ALL TODOS
+    if (req.url.match(/\/api\/todos/) && req.method === 'GET')
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(todos))
+  })
+  .listen(8082)
+console.log('Server is Running on port 8082')
